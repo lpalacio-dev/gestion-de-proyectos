5b6222a33a360ba3b335402b012a9c6f13c67393
Luis Alberto Pérez Palacio <luispalacio.ap@gmail.com>
2026-02-04 16:28:07 -0600

    Instalación del SDK de aws S3

--- Program.cs
+++ Program.cs
@@ -8,13 +8,20 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Amazon.S3;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. CARGAR VARIABLES DE ENTORNO DESDE .ENV
+Env.Load();
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
+
+builder.Services.AddAWSService<IAmazonS3>();
 
 // Registrar los servicios de Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
